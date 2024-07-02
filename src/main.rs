30e83737b446ebb99a07711898589ea9a1699d3d
Armaan Gupta <ohayouarmaan@gmail.com>
2024-07-02 18:45:32 +0530

    feat: reading files

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,23 @@
+use std::{fs,env};
+
 mod lexer;
 mod parser;
 mod transpiler;
 
 fn main() {
-    let json_string = r#"
-{"name":{"first_name":"Armaan","last_name":"Gup\"ta","employement":{"test_number":50000,"role":"software engineer","test_boolean":false,"please work":{},"test_null":null,"test_array":["wow123",{"a":"b"}]}}}
-    "#.to_string();
-
-    let mut l = lexer::Lexer::new(json_string);
-    l.lex();
-    let mut p = parser::Parser::new(&l.tokens);
-    p.parse();
-    if let Some(root_object) = p.root_object {
-        let t = transpiler::Transpiler::new(&root_object);
-        t.format();
+    let arguments = env::args().collect::<Vec<String>>();
+    let first_argument = arguments.get(1);
+    if let Some(file_name) = first_argument {
+        let json_string = fs::read_to_string(file_name).expect("[Error]: Can not open specified file.");
+        let mut l = lexer::Lexer::new(json_string);
+        l.lex();
+        let mut p = parser::Parser::new(&l.tokens);
+        p.parse();
+        if let Some(root_object) = p.root_object {
+            let t = transpiler::Transpiler::new(&root_object);
+            t.format();
+        }
+    } else {
+        println!("No file provided");
     }
 }
